3b33d873ac7bc6ccc494ca756e89b6b6e9cb7794
Rathod-Pratik <rathodpratik1928@gmail.com>
2025-04-13 23:11:36 +0530

    Add home icon

--- Fronted/src/Components/AdminNavbar.jsx
+++ Fronted/src/Components/AdminNavbar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAppStore } from "../Store";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
 const AdminNavbar = () => {
   const { userInfo } = useAppStore();
-
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className="bg-white border-b-gray-200 border-b py-3">
-      <div className="flex justify-between items-center w-[90vw] mx-auto">
+    <div className="backdrop-blur-lg border-b border-gray-200 py-3 sticky top-0 z-50">
+      <div className="flex justify-between items-center mx-auto lg:w-[90vw]">
         {/* Logo */}
         <Link to="/admin">
           <img
@@ -20,12 +28,14 @@ const AdminNavbar = () => {
         </Link>
 
         {/* User Section */}
-        <div className="relative">
-          <p
-            className="rounded-full text-[orange] px-4 py-2"
-          >
+        <div className="relative flex gap-2 items-center">
+          <p className="rounded-full text-[orange] px-4 py-2">
             Welcome, {userInfo.name}
           </p>
+          <Link to="/" className="p-2 rounded-full bg-orange-500 hover:bg-orange-600 transition">
+  <FaHome className="text-white text-2xl" />
+</Link>
+
         </div>
       </div>
     </div>
