9a3b4131f95d6b11ad70ad48ef4131f1a3feee75
Rathod-Pratik <rathodpratik1928@gmail.com>
2026-08-31 15:58:45 +0530

    remove code for fronted and backend

--- Backend/middlewares/multer.middleware.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-// Use memory storage to keep file in memory, not on disk
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage });
-
-export default upload;
--- Fronted/src/lib/api-Client.js
+++
@@ -1,6 +0,0 @@
-import   axios  from "axios";
-import { HOST } from "../Utils/Constant";
-
-export const apiClient=axios.create({
-    baseURL:HOST
-})
