67208a25a4aa3b90568aa4467a51516ded358435
Rathod-Pratik <rathodpratik1928@gmail.com>
2026-08-31 16:12:55 +0530

    remove the application for the dev branch

--- app/src/app/(public)/home/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Experience";
-export * from "./Gallery";
-export * from "./Hero";
-export * from "./Services";
-export * from "./Tour";
-export * from "./Testimonial";
--- app/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./constants";
-export * from "./Functions";
-export * from "./validations";
